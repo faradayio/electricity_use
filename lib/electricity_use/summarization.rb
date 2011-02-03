ca74072c784fefb294e012e208f1ce3efbfab3fb
Ian Hough <ian@brighterplanet.com>
2011-02-03 08:49:18 -0800

    Aspect is :perfect; cut unused stuff from summarization

--- lib/electricity_use/summarization.rb
+++ lib/electricity_use/summarization.rb
@@ -6,16 +6,8 @@ module BrighterPlanet
       def self.included(base)
         base.extend SummaryJudgement
         base.summarize do |has|
-#          has.adjective 'one-way', :if => lambda { |flight| flight.trips == 1 }
-#          has.adjective 'round-trip', :if => lambda { |flight| flight.trips == 2 }
-#          has.adjective 'nonstop', :if => lambda { |flight| flight.segments_per_trip == 1 }
-#          has.identity 'flight'
-#          has.modifier lambda { |flight| "from #{flight.origin_airport.name}" }, :if => :origin_airport
-#          has.modifier lambda { |flight| "to #{flight.destination_airport.name}" }, :if => :destination_airport
-#          has.modifier lambda { |flight| "on a #{flight.vehicle}" }, :if => :vehicle
-#          has.modifier lambda { |flight| "on #{flight.date.to_formatted_s(:archive)}"}, :if => :date
           has.verb :use
-          has.aspect :imperfect
+          has.aspect :perfect
         end
       end
     end
