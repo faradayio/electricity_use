8de76c1ed29f3e2eb10e68d072c04b9716e0a275
Ian <ian@worklap.local>
2011-09-07 19:37:43 +0200

    Log test output

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,4 +8,4 @@ require 'data_miner'
 DataMiner.logger = Logger.new(nil)
 
 require 'sniff'
-Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :locality, :cucumber => true
+Sniff.init File.join(File.dirname(__FILE__), '..', '..'), :earth => :locality, :cucumber => true, :logger => 'log/test_log.txt'
